4da7aac3e9397dd8c562f071e6faf421c8dc1706
Victor Twu <victor607@gmail.com>
2026-04-26 13:31:36 -0700

    Initial CDK scaffold

---
+++ lib/data-management-cdk-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class DataManagementCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Define resources here
+  }
+}
